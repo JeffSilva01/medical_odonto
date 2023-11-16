8952e7ef53a5812d6daf8470bf5676857a41cb81
Jefferson Silva <jeffsilva01.dev@gmail.com>
2023-11-16 16:16:03 -0300

    video pause

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -7,9 +7,13 @@ export function Video() {
   const [isPlay, setIsPlay] = useState(false)
 
   function handlerPlay() {
-    videoRef.current?.play()
+    if (isPlay) {
+      videoRef.current?.pause()
+    } else {
+      videoRef.current?.play()
+    }
 
-    setIsPlay(true)
+    setIsPlay(!isPlay)
   }
 
   return (
@@ -19,14 +23,12 @@ export function Video() {
         className="h-[550px] w-[300px]"
         src="/video.MP4"
       ></video>
-      {!isPlay && (
-        <button
-          onClick={handlerPlay}
-          className="absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center "
-        >
-          <PlayIcon className="text-white" size={50} />
-        </button>
-      )}
+      <button
+        onClick={handlerPlay}
+        className="absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center "
+      >
+        {!isPlay && <PlayIcon className="text-white" size={50} />}
+      </button>
     </div>
   )
 }
